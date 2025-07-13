a51a291c4f4ce4d7b5d2aaa561041073c819899c
LexyArgento <lexyargento6@gmail.com>
2025-07-13 01:10:18 -0300

    interactive.sh
    - Fix bugs
    - NO code inyection

--- interactive.sh
+++ interactive.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
+echo "ejecuta 'n-help' para ayuda del script y '-h' o '--help' para ayuda de nmap"
 while true; do
-     read -p "nmap> " comando
-     if [[ "$comando" == "exit" || "$comando" == "quit" ]]; then
-        break
-     fi
-     nmap $comando
+     read -p "nmap> " -a comando
+     [[ -z "${comando[0]}" ]] && continue
+     case "${comando[0]}" in
+         "exit"|"quit") break;;
+         "n-help") echo "comandos validos: scan <taget>, exit/quit y todos los argumentos de nmap"
+                  ;;
+         *) nmap "${comando[@]}"
+            ;;
+      esac
 done
