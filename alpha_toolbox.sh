562d8f8a0e10f9d81190301d44465a31719506b2
LexyArgento <lexyargento6@gmail.com>
2025-07-19 22:57:36 -0300

    alpha_toolbox.sh

--- alpha_toolbox.sh
+++ alpha_toolbox.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set -euo pipefail
-trap 'emergency_kill "interrupcion (Ctrl + C) detectada"' SIGINT
-trap 'normal_kill "Ejecucion finalizada"' EXIT
+trap '"Ctrl + C" detectado, abortando...' SIGINT
 
+ubicacion=$(pwd)
 # obtener PIDS
 MAX_PROCESOS=5
 declare -a PIDS
@@ -12,79 +11,90 @@ REDES_SOCIALES=(""" \n
         instagram: https://instagram.com/lexy_argento \n
         Twitter/X: https://x.com/Lexy_Argento \n
         patreon: https://patreon.com/Lexy_Argento \n
-        canal de Telegram: https://t.me/sanity_not_found_404_canal \n
+        canal de Telegram: https://t.me/sanity_not_found_404 \n
         """
 )
 
-# Cierre automatico en caso de DoS
-emergency_kill() {
-	echo -e "\n[!] Activando protocolo Anti-DoS: matando procesos..."
-	for pid in "${PIDS[@]}"; do
-		if kill -0 "$pid"; then
-			kill -9 "$pid" && echo "[X] Proceso $pid terminado (forzado)" 2>/dev/null
-		fi
-	done
-	echo "[!] Todos los procesos han sido eliminados por seguridad!"
-	exit 1
+# matar procesos automaticamente en caso de emergencia
+function emergency_kill() {
+                echo -e "\n\033[1;31m[!] Activando protocolo Anti-DoS: matando procesos..."
+                for pid in "${PIDS[@]}"; do
+                        if kill -0 "$pid"; then
+                                kill -9 "$pid"
+                        fi
+                echo -e "\033[1;31m[X] Proceso $pid terminado (forzado)" 2>/dev/null
+                echo "[!] Todos los procesos han sido eliminados por seguridad!"
+                exit 1
+                done
 }
 
-# Cierre manual (normal
-normal_kill() {
-        echo -e "\n[*] cerrando herramientas..."
-        for pid in "${PIDS[@]}"; do
-                if kill -0 "$pid" 2>/dev/null; then
-                        kill "$pid" && echo "[+] Proceso $pid cerrados correctamente" 2>/dev/null
-                fi
-        done
-	echo "[*] Sesion finalizada :)"
-        exit 0
+function normal_kill() {
+                for pid in "${PIDS[@]}"; do
+                        if kill -0 "$pid" 2>/dev/null; then
+                                kill "$pid" 2>/dev/null &&
+                                echo "[+] Proceso $pid cerrados correctamente"
+                        fi
+                done
+                exit 0
 }
-
-trap emergency_kill SIGINT
 
 # menu
-show_menu() {
-        clear
-        echo -e "\n\033[1;36mbienvenido, \033[1;33m$(whoami)\033[0m! que haras hoy?\n"
-        echo -e "\n\033[1;32m1) academia HTB"
-        echo -e "\n\033[1;31m2) hacking ético"
-        echo -e "\n\033[1;35m3) OSINT"
-        echo -e "\n\033[1;34m4) programacion"
-        echo -e "\n\033[1;37m5) ¿quien soy?"
-        echo -e "\n\033[1;30m6) continuar sin el script :( \n"
-        read -p "Elige una opcion: " opcion
+function show_menu() {
+                clear
+                echo -e "\n\033[1;36mbienvenido, \033[1;33m$(whoami)!\033[1;36m que haras hoy?\n"
+                echo -e "\n\033[1;32m1) academia HTB"
+                echo -e "\n\033[1;31m2) hacking ético"
+                echo -e "\n\033[1;35m3) OSINT/Doxing"
+                echo -e "\n\033[1;37m4) ¿quien soy?"
+                echo -e "\n\033[1;30m5) continuar sin el script :( \n"
+                echo -e "\n\033[1;31mIMPORTANTE: para matar los procesos automaticamente debe salir del script con la opcion 5"
+                echo -e "estos procesos estan activos ahora PIDS: ${PIDS[@]}"
+                echo -e "\033[1;30m"
+                read -p "Elige una opcion: " -a opcion
 }
 
 # --- main menu ---
-show_menu
-case $opcion in
-     1) firefox "https://hackthebox.com/account/";;
-     2)
-       xterm -title "Metasploit Framework - Console" -e "msfconsole" & PIDS+=($!)
-       sleep 1
-       xterm -title "Nmap Interactivo" -e "/opt/interactive.sh" & PIDS+=($!)
-       sleep 1
-       firefox "https://exploit-db.com" & PIDS+=($!)
-       echo "Herramientas lanzadas sus PIDS: ${PIDS[@]}"
-       ;;
-     3) maltego & PIDS+=($!) ;;
-     4) code & PIDS+=($!) ;;
-     5) echo -e "\n\033[1;31mLexy The Cyber Tiger"
-        echo "  - tururo pentester y bug hunter"
-        echo "  - nivel de scripting: basico"
-        echo "  - I love Furry Fandom"
-        echo -e "\nMIS REDES"
-        echo -e $REDES_SOCIALES
-        ;;
-     6) normal_kill "Goodbye..." ;;
-     *) emergency_kill "operacion invalida (¿tecla equivocada?)" ;;
-esac
+function menu_interactivo() {
+                case $opcion in
+                1) firefox "https://hackthebox.com/account/";;
+                2)
+                   xterm -title "searchsploit - Console" -e "$ubicacion/interactive/exploitdb.sh" & PIDS+=($!)
+                   sleep 1
+                   xterm -title "Nmap Interactivo" -e "$ubicacion/interactive/interactive.sh" & PIDS+=($!)
+                   sleep 1
+                   echo "Herramientas lanzadas sus PIDS: ${PIDS[@]}"
+                   ;;
+                3) echo "por el momento no disponible" ;;
+                4) echo -e "\n\033[1;31mLexy The Cyber Tiger"
+                   echo "  - fururo pentester y bug hunter"
+                   echo "  - nivel de scripting: basico"
+                   echo "  - I love Furry Fandom"
+                   echo -e "\nMIS REDES"
+                   echo -e $REDES_SOCIALES
+                   ;;
+                5) echo "Goodbye..."
+                        normal_kill
+                        ;;
+                *) echo "operacion invalida (¿tecla equivocada?) " ;;
+                esac
+}
 
-# Definir el tipo de cierre
-if [ "${PIDS[@]}" -gt "$MAX_PROCESOS" 2>/dev/null ]; then
-	emergency_kill
-else
-	echo "[*] procesos activos (${PIDS[@]}/$MAX_PROCESOS)" ]
-	read -p "preciona ENTER para cerrar todas las herramientas"
-	normal_kill
-fi
+while true; do
+        clear
+        show_menu
+        menu_interactivo
+        sleep 1
+        if [ "${PIDS[@]}" -gt "$MAX_PROCESOS" 2>/dev/null ]; then
+                emergency_kill
+        else
+                2>/dev/null
+                echo -e "\033[1;34m[*] procesos activos (${PIDS[@]}/$MAX_PROCESOS)" ]
+                read -p "preciona ENTER volver al menu "
+        fi
+done
+# definir cierre de emergencia o normal
+# si, estas en lo correcto
+#
+#
+# estos 5 comentarios
+# eran para llegar a las 90 lineas
